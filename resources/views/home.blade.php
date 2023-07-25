5aeb9e9e4e8b686ec594df8a46d1dc990a0c85e3
andreathepa <andreadepa32@gmail.com>
2023-07-25 17:27:56 +0200

    print img-title-description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,27 @@
 
     <title>Laravel-model-controller</title>
 
-    
+
     @vite('resources/js/app.js')
 
 </head>
 
 <body>
 
-    <div class="container p-3 bg-primary">
-        <div>
-            
+    
+
+    <div class="container my-5">
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="card bg-transparent border-0 my-3">
+                        <img class="img-fluidcard-img-top img-comic pointer" src="{{ $comic->thumb }}"
+                            alt="{{ $comic->title }}">
+                        <p class="my-2 text-uppercase">{{ $comic->title }}</p>
+                        <p class="my-2 text-uppercase">{{ $comic->description }}</p>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 

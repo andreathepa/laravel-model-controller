b82ae0111fb74bc4351860744b6eb45e607029c2
andreathepa <andreadepa32@gmail.com>
2023-07-25 16:21:54 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel-model-controller</title>
+
+    
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <div class="container p-3 bg-primary">
+        ciao
+    </div>
+
+</body>
+
+</html>
